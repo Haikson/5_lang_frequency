4579d826532e4a00b47b4a9acc0802f4070a391c
Kamo Petrosyan <kamo@haikson.com>
2017-01-16 04:30:20 +0000

    modified:   lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,32 @@
+import sys
+import re
+import codecs
+import operator
 
+def remove_punctuation(text):
+    return re.sub(r"\W+", " ", text).lower()
 
 def load_data(filepath):
-    pass
+    with codecs.open(filepath, 'r', 'utf-8') as fd:
+        text = fd.read()
+    return text
 
 
 def get_most_frequent_words(text):
-    pass
+    words_dict = {}
+    for word in text.split():
+        if word == '':
+            pass
+        words_count = 1 + words_dict.get(word, 0)
+        words_dict[word] = words_count
+    sorted_words = sorted(words_dict.items(), key=operator.itemgetter(1), reverse=True)
+    
+    for dict_item in sorted_words[:10]:
+        print(u"{}".format(dict_item[0]))
+        
 
 
 if __name__ == '__main__':
-    pass
+    text = load_data(sys.argv[1])
+    text = remove_punctuation(text)
+    get_most_frequent_words(text)
